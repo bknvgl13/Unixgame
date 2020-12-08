5bf1e68075b98f6544541350709284a07fb7a9f5
bknvgl13 <39342526+bknvgl13@users.noreply.github.com>
2020-12-09 00:45:23 +0300

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,27 @@
+#!/usr/bin/env bash
 
+#Algorithm: first, the number of files in the directory is calculated, second the expected number is chosen. 
+#Then there is an equality comparison. The programm ends when the user guesses the number correctly.
+
+num=$(ls | wc -l)
+
+function forecast_file {
+	echo "Enter your guess for the number of files in directory:"
+	read forecast
+	while [[ $forecast -ne num ]] 
+	do 
+	if [[ $forecast -gt num ]]
+		then
+			echo "Too much, press Enter:"
+			read forecast
+		elif [[ $forecast -lt num ]] 
+		     then
+			echo "Too less, press Enter:"
+			read forecast
+		fi
+	done
+	}
+	
+	echo "Forecast how many files are in this directory:"
+	forecast_file 
+	echo "Right!"
